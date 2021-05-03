ff280b5932d742d889580fa9d18319cd935aaec9
God Calls Me God <82312111+godcallsmegod@users.noreply.github.com>
2021-05-03 23:37:19 +0800

    Update app.js

--- app.js
+++ app.js
@@ -123,6 +123,11 @@ app.get('/v2api/song/url', async (req, res) => {
       path: `/song/media/outer/url?id=${req.query.id}`,
       method: 'GET',
       headers: {
+        'accept-encoding': 'gzip, deflate, br',
+        'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8'
+        'content-type': 'application/x-www-form-urlencoded',
+        'origin': 'https://music.163.com',
+        'referer': 'https://music.163.com/',
         'user-agent': 'Mozilla/5.0 (Windows 10) AppleWebKit/537.36 (KHTML, like Gecko) Safari/537.36',
         'X-Forwarded-For': '223.72.142.221',
         'X-Real-IP': '223.72.142.221'
@@ -130,9 +135,6 @@ app.get('/v2api/song/url', async (req, res) => {
     }, (_res) => {
       _url = _res.headers.location
       res.json({short: _shortURL, url: _url})
-      
-      console.log('状态码:', res.statusCode)
-      console.log('请求头:', res.headers)
     })
   }else{
     res.json({error: true})
